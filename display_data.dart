cd05bb48f55e65194a0affd1c73dc1ead9cef5b4
Vaibhav Rana <vaibhavr9495@gmail.com>
2026-07-15 09:32:19 +0530

    display_data.dart

---
+++ display_data.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../person_model.dart';
+
+class Api {
+  static const baseurl = "http://192.168.1.223:3000/api/";
+
+  //POST api
+  static addperson (Map pdata) async {
+    print(pdata);
+
+    var url = Uri.parse(baseurl + "add_person");
+
+    try {
+
+      final res = await http.post(url, body: pdata);
+
+      if (res.statusCode == 200) {
+
+        var data = jsonDecode(res.body.toString());
+        print(data);
+
+      } else {
+        print("Failed to upload data");
+      }
+    } catch (e){
+      print(e.toString());
+    }
+  }
+
+  //GET api
+  static getPerson() async {
+    List<Person> person = [ ];
+    var url = Uri.parse(baseurl + "get_person");
+
+    try{
+      final res = await http.get(Uri.parse("uri"));
+
+      if (res.statusCode == 200) {
+        var data = jsonDecode(res.body);
+        print(data);
+
+        data['person']?.forEach((value)=>{
+          person.add(
+            Person(
+              name: value['pname'],
+                phone: value['pphone'],
+                age: value['page'],
+            )
+          )
+        });
+        return person;
+
+      } else { return [];}
+
+    } catch(e) {
+      print(e.toString());
+    }
+  }
+}
