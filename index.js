d1d32d2def9ab00a3bec2f1c177d9612fe89ae79
Vaibhav Rana <vaibhavr9495@gmail.com>
2026-07-15 09:29:18 +0530

    index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.listen( port, ()=> {
 
 
 
-//post api
+//POST api
 
 app.post("/api/add_person",(req,res) => {
         console.log("Result: ", req.body);
@@ -44,3 +44,19 @@ app.post("/api/add_person",(req,res) => {
         });
 
 })
+
+//GET api
+app.get("/api/get_person",(req,res) => {
+
+        if(personData.length>0) {
+                res.status(200).send ({
+                        "Status_code" : 200,
+                        "Person" : [],
+                        
+                        
+                })
+        }
+
+
+});
+
