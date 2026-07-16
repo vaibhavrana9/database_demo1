55f8e7ccaa4eefaba054022892f26b5abdc6d919
Vaibhav Rana <vaibhavr9495@gmail.com>
2026-07-16 09:23:43 +0530

    Update display_data.dart

--- display_data.dart
+++ display_data.dart
@@ -1,61 +1,66 @@
-import 'dart:convert';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import '../person_model.dart';
+import 'package:database_2/person_model.dart';
+import 'package:database_2/services/api.dart';
+import'package:flutter/material.dart';
 
-class Api {
-  static const baseurl = "http://192.168.1.223:3000/api/";
+class DisplayData extends StatefulWidget {
+  const DisplayData({super.key});
 
-  //POST api
-  static addperson (Map pdata) async {
-    print(pdata);
+  @override
+  State<DisplayData> createState() => _DisplayDataState();
+}
 
-    var url = Uri.parse(baseurl + "add_person");
+class _DisplayDataState extends State<DisplayData> {
 
-    try {
 
-      final res = await http.post(url, body: pdata);
 
-      if (res.statusCode == 200) {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Display Data"),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
 
-        var data = jsonDecode(res.body.toString());
-        print(data);
+      body: FutureBuilder(
+        future: Api.getPerson(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final pdata = snapshot.data!;
 
-      } else {
-        print("Failed to upload data");
-      }
-    } catch (e){
-      print(e.toString());
-    }
-  }
+            return ListView.builder(
+              itemCount: pdata.length,
+              
+              itemBuilder: (context, index) {
+                return ListTile(
+                  minVerticalPadding: 14,
+                  //icon
+                  leading: const Icon(Icons.person),
 
-  //GET api
-  static getPerson() async {
-    List<Person> person = [ ];
-    var url = Uri.parse(baseurl + "get_person");
+                  //name
+                  title: Text("Name : ${pdata[index].name}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18),),
 
-    try{
-      final res = await http.get(Uri.parse("uri"));
+                  //phone
+                  subtitle: Text("Phone : ${pdata[index].phone}"),
 
-      if (res.statusCode == 200) {
-        var data = jsonDecode(res.body);
-        print(data);
+                  //age
+                  trailing: Text("Age : ${pdata[index].age}",
+                    style: TextStyle(
+                      fontSize: 16),),
 
-        data['person']?.forEach((value)=>{
-          person.add(
-            Person(
-              name: value['pname'],
-                phone: value['pphone'],
-                age: value['page'],
-            )
-          )
-        });
-        return person;
-
-      } else { return [];}
-
-    } catch(e) {
-      print(e.toString());
-    }
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text("No Data Found"),
+            );
+          }
+        },
+      ),
+    );
   }
 }
